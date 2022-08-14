c75fa019cd76c53ae142040c4c0fbfbec9efa14c
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-08-14 16:14:02 +0900

    java_spring study: post

---
+++ post/src/main/java/com/example/post/dto/PostRequestDto.java
@@ -0,0 +1,79 @@
+package com.example.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostRequestDto {
+
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+
+    // snake_case 와 camelCase
+    // 보내는 변수이름은 snake_case면 매칭 못함
+    // @JsonProperty("phone_number") 사용해야함
+    @JsonProperty("phone_number")
+    private String phoneNumber; // phone_number (
+    @JsonProperty("OTP")
+    private String OTP;
+
+    public String getOTP() {
+        return OTP;
+    }
+
+    public void setOTP(String OTP) {
+        this.OTP = OTP;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PostRequestDto{" +
+                "account='" + account + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", OTP='" + OTP + '\'' +
+                '}';
+    }
+}
