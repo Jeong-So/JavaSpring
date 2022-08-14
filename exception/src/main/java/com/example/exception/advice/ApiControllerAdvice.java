cc476bd5064c1ff4911b5d5890f98be379dcf650
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-08-14 22:32:39 +0900

    Spring Boot Exception 및 Validation 실습_중간 Commit

---
+++ exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
@@ -0,0 +1,98 @@
+package com.example.exception.advice;
+
+import com.example.exception.controller.ApiController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+//@ControllerAdvice  // View 사용 시 시용하는 advice
+// 패키지 지정 시 해당 패키지 하위 예외 다 잡음
+//@RestControllerAdvice(basePackages = "com.example.exception.controller") // Rest Controller 사용시 시용하는 advice (전역 Advice)
+@RestControllerAdvice(basePackageClasses = ApiController.class) // ApiController에서만 동작하는 Advice
+public class ApiControllerAdvice {
+
+    // RestApii 는 ResponseEntity를 뱉음
+    @ExceptionHandler(value = Exception.class) // 모든 예외를 다 잡음
+    public ResponseEntity exception(Exception e) {
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    // 특정 Method의 예외를 잡을 시
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError field = (FieldError) error;
+
+            String fieldName = field.getField();
+            String message = field.getDefaultMessage();
+            String value = field.getRejectedValue().toString();
+
+            System.out.println("----------------------");
+            System.out.println(fieldName);
+            System.out.println(message);
+            System.out.println(value);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity constraintViolationException(ConstraintViolationException e){
+
+        e.getConstraintViolations().forEach(error ->{
+
+            Stream<Path.Node> stream = StreamSupport.stream(error.getPropertyPath().spliterator(), false);
+            List<Path.Node> list = stream.collect(Collectors.toList());
+
+//            String field = error.getLeafBean().toString();
+            String field = list.get(list.size() -1).getName();
+            String message = error.getMessage();
+            String invalidValue = error.getInvalidValue().toString();
+
+            System.out.println("----------------------");
+            System.out.println(field);
+            System.out.println(message);
+            System.out.println(invalidValue);
+
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e){
+
+        String fieldName = e.getParameterName();
+        String fieldType = e.getParameterType();
+        String invalidValue = e.getMessage();
+
+        System.out.println(fieldName);
+        System.out.println(fieldType);
+        System.out.println(invalidValue);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    }
+
+
+
--- exception/src/main/java/com/example/exception/controller/ApiController.java
+++ exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -3,22 +3,33 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping ("")// required = false : 값이 없더라도 동작시킴
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam(required = true) String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam(required = true) Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
 
-        int a = 10+age;
+//        int a = 10+age;
 
         return user;
     }
@@ -32,10 +43,10 @@ public class ApiController {
     // Controller 안에 작성 시
     // 이 @ExceptionHandler 는 이 컨트롤러 안에서 일어나는 것만 관여
     // Global 에 지정된 @ExceptionHandler보다 컨트롤러 안에 지정된게 우선순위가 높음
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
-        System.out.println("api controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
+//        System.out.println("api controller");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
 
 }
