0353ea10f82d6fd1ffc7529f0cbc8c6e844be6bf
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-08-18 00:00:51 +0900

    java_spring study: filter_최종

---
+++ filter/src/main/java/com/example/filter/controller/ApiUserController.java
@@ -0,0 +1,20 @@
+package com.example.filter.controller;
+
+import com.example.filter.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j  // lombok 사용 시 : System.out.println()  -->  log // lombok 아니더라도 설정 가능
+@RestController
+@RequestMapping("/api/temp")
+public class ApiUserController {
+
+    @PostMapping("")
+    public User user(@RequestBody User user){
+        log.info("Temp : {}, {}", user, user);
+        return user;
+    }
+}
